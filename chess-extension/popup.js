132f546674828fd043619aa036fc4e96440af0c5
Kamlesh-S-B <kamlesh.s.bhavsar@gmail.com>
2026-05-20 18:07:39 -0500

    Added storage for counter
    Counter now saves value using chrome.storage.local so it does not reset on popup close.

--- chess-extension/popup.js
+++ chess-extension/popup.js
@@ -1,9 +1,34 @@
+
 let count = 0;
+async function setClicks()
+{
+    await chrome.storage.local.set({numClicks: count});
+}
+
+async function setCount()
+{
+    let num = await chrome.storage.local.get(["numClicks"]);
+
+    num = num.numClicks;
+    console.log(num);
+    if(num>0)
+    {
+        count = num;
+        document.getElementById("displayClicks").textContent = "Times Clicked: " + count;
+
+    }
+    
+}
 
 
-function getClicks()
+setCount();
+
+
+
+function addClick()
 {
     count++;
     document.getElementById("displayClicks").textContent = "Times Clicked: " + count;
+    setClicks();
 }
-document.getElementById("button1").addEventListener("click", getClicks)
+document.getElementById("button1").addEventListener("click", addClick)
