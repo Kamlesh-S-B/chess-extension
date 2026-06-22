e4adea4e706e3ff373e6e6c09beb7eb33ea46b39
Kamlesh-S-B <kamlesh.s.bhavsar@gmail.com>
2026-06-22 12:25:53 -0500

    Tree Health, Win Rate, and UI Refactor
    Huge update. Added win rate calculation that signifies tree health status (either Thriving, Growing, or Struggling). Simplified old functions by creating  new functions like calculate Win rate and getTree level. Rewored UpdateTree into updateUI. Improved overall code organization and readability.

--- chess-extension/chess-extension/popup.js
+++ chess-extension/chess-extension/popup.js
@@ -1,180 +1,79 @@
-let totalGames = 0;
-let gamesWon = 0;
-let gamesLost = 0;
-let gamesDrawn = 0;
-
-let username="";
-
-
-async function setStats()
+function updateUI(stats, winrate)
 {
-    let num = await chrome.storage.local.get(["gameCount"]);
-    num = num.gameCount;
-    if(num>0)
-    {
-        totalGames = num;
-        document.getElementById("total-games").textContent = "Total Games: " + totalGames;
-
-    }
-
-    num = await chrome.storage.local.get(["winCount"]);
-    num = num.winCount;
-    if(num > 0)
-    {
-        gamesWon = num;
-        document.getElementById("games-won").textContent = "Games Won: " + gamesWon;
-
-    }
-
-    num = await chrome.storage.local.get(["lossCount"]);
-    num = num.lossCount;
-    if(num > 0)
-    {
-        gamesLost = num;
-        document.getElementById("games-lost").textContent = "Games Lost: " + gamesLost;
-
-    }
-
-    num = await chrome.storage.local.get(["drawCount"]);
-    num = num.drawCount;
-    if(num > 0)
-    {
-        gamesDrawn = num;
-        document.getElementById("games-drawn").textContent = "Games Drawn: " + gamesDrawn;
-
-    }
-
-    updateTree()
-}
-setStats();
-
-
-
-//ON button click
-function addWin()
-{
-    totalGames++;
-    gamesWon++;
-    chrome.storage.local.set({winCount: gamesWon});
-    chrome.storage.local.set({gameCount: totalGames});
     
-
-    document.getElementById("games-won").textContent = "Games Won: " + gamesWon;
-    document.getElementById("total-games").textContent = "Total Games: " + totalGames;
-    updateTree();
-     
-}
-function addLoss()
-{
-    totalGames++;
-    gamesLost++;
-    chrome.storage.local.set({lossCount: gamesLost});
-    chrome.storage.local.set({gameCount: totalGames});
-    
-
-
-    document.getElementById("games-lost").textContent = "Games Lost: " + gamesLost;
-    document.getElementById("total-games").textContent = "Total Games: " + totalGames;
-    updateTree();
-}
-function addDraw()
-{
-    totalGames++;
-    gamesDrawn++;
-    chrome.storage.local.set({drawCount: gamesDrawn});
-    chrome.storage.local.set({gameCount: totalGames});
-    
-
-
-    document.getElementById("games-drawn").textContent = "Games Drawn: " + gamesDrawn;
-    document.getElementById("total-games").textContent = "Total Games: " + totalGames;
-    updateTree()
-}
-
-function clearAll()
-{
-    totalGames = 0;
-    gamesWon = 0;
-    gamesLost = 0;
-    gamesDrawn = 0;
-    chrome.storage.local.set({gameCount:0});
-    chrome.storage.local.set({winCount:0});
-    chrome.storage.local.set({lossCount:0});
-    chrome.storage.local.set({drawCount:0});
-
-    document.getElementById("total-games").textContent = "Total Games: " + totalGames;
-    document.getElementById("games-won").textContent = "Games Won: " + gamesWon;
-    document.getElementById("games-lost").textContent = "Games Lost: " + gamesLost;
-    document.getElementById("games-drawn").textContent = "Games Drawn: " + gamesDrawn;
-    updateTree()
-
-}
-
-async function updateTree()
-{
-    let currentTree = document.getElementById("tree-image");
-    if(totalGames<10)
-    {
-        document.getElementById("games-to-next").textContent=(10-totalGames) + " Games To Next Tree";
-        document.getElementById("tree-level").textContent ="Current Tree: Seed";
-        currentTree.src="https://openmoji.org/data/color/svg/1F330.svg";
-    }
-    if(totalGames>=10 && totalGames<25)
-    {
-        document.getElementById("games-to-next").textContent=(25-totalGames) + " Games To Next Tree";
-        document.getElementById("tree-level").textContent ="Current Tree: Sapling";
-        currentTree.src="https://openmoji.org/data/color/svg/1F331.svg";
-    }
-    if(totalGames>=25 && totalGames<50)
-    {
-        document.getElementById("games-to-next").textContent=(50-totalGames) + " Games To Next Tree";
-        document.getElementById("tree-level").textContent ="Current Tree: Young Tree";
-        currentTree.src="https://openmoji.org/data/color/svg/1F33F.svg";
-    }
-    if(totalGames>=50 && totalGames<100)
-    {
-        document.getElementById("games-to-next").textContent=(100-totalGames) + " Games To Next Tree";
-        document.getElementById("tree-level").textContent ="Current Tree: Mature Tree";
-        currentTree.src="https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg/1f333.svg"
-    }
-    if(totalGames>=100)
-    {
-        document.getElementById("games-to-next").textContent= "Forest Achieved!"
-        document.getElementById("tree-level").textContent = "Current Tree: Forest";
-        currentTree.src="https://imgproxy.attic.sh/insecure/f:webp/h:993/q:90/w:993/plain/https://attic.sh/qkqlry3bju95ui8tue1acaz07v6g";
-    }
-}
-
-
-async function onEnter()
-{
-    username = document.getElementById("username-input").value;
-    await chrome.storage.local.set({username:username})
-
-    let stats = await getLichessStats(username);
-    console.log(stats)
-
-
-    totalGames = stats.all;
-    gamesWon = stats.win;
-    gamesLost = stats.loss;
-    gamesDrawn = stats.draw;
-
-
-    await chrome.storage.local.set({
-    gameCount: stats.all,
-    winCount: stats.win,
-    lossCount: stats.loss,
-    drawCount: stats.draw
-});
-
+    let treeLevel = getTreeLevel(stats);
     document.getElementById("total-games").textContent = "Total Games: " + stats.all;
     document.getElementById("games-won").textContent = "Games Won: " + stats.win;
     document.getElementById("games-lost").textContent = "Games Lost: " + stats.loss;
     document.getElementById("games-drawn").textContent = "Games Drawn: " + stats.draw;
-    
-    await updateTree();
+
+
+    let health = "Struggling";
+    if(winrate >=60)
+    {
+        health = "Thriving";
+    }
+    else if(winrate >=45)
+    {
+        health = "Growing";
+    }
+
+    document.getElementById("tree-level").textContent = `${treeLevel} | ${health} | WR: ${winrate}%`;
 }
+
+function getTreeLevel(stats)
+{
+    let currentTree = document.getElementById("tree-image");
+    if(stats.all<10)
+    {
+        document.getElementById("games-to-next").textContent=(10-stats.all) + " Games To Next Tree";
+        currentTree.src="https://openmoji.org/data/color/svg/1F330.svg";
+        return "Seed";
+    }
+    if(stats.all>=10 && stats.all<25)
+    {
+        document.getElementById("games-to-next").textContent=(25-stats.all) + " Games To Next Tree";
+        currentTree.src="https://openmoji.org/data/color/svg/1F331.svg";
+        return "Sapling";
+    }
+    if(stats.all>=25 && stats.all<50)
+    {
+        document.getElementById("games-to-next").textContent=(50-stats.all) + " Games To Next Tree";
+        currentTree.src="https://openmoji.org/data/color/svg/1F33F.svg";
+        return "Young Tree";
+    }
+    if(stats.all>=50 && stats.all<100)
+    {
+        document.getElementById("games-to-next").textContent=(100-stats.all) + " Games To Next Tree";
+        currentTree.src="https://images.emojiterra.com/google/noto-emoji/unicode-17.0/color/svg/1f333.svg"
+        return "Mature Tree";
+    }
+    if(stats.all>=100)
+    {
+        document.getElementById("games-to-next").textContent= "Forest Achieved!"
+        currentTree.src="https://imgproxy.attic.sh/insecure/f:webp/h:993/q:90/w:993/plain/https://attic.sh/qkqlry3bju95ui8tue1acaz07v6g";
+        return "Forest";
+    }
+
+}
+
+
+
+async function onEnter()
+{
+    let username = document.getElementById("username-input").value;
+    
+    await chrome.storage.local.set({username:username})
+
+    let stats = await getLichessStats(username);
+
+    let winRate =calculateWinRate(stats);
+
+    
+     updateUI(stats, winRate);
+}
+
+
 
 
 
@@ -204,44 +103,34 @@ function checkKey(event)
     }
 }
 
+function calculateWinRate(stats)
+{
+    let winRate = (stats.win/stats.all) *100;
+    winRate =  Math.round(winRate*100)/100;
+    return winRate;
+}
+
 async function loadUser()
 {
+
     let result = await chrome.storage.local.get("username");
     username = result.username
-    
-    document.getElementById("username-input").value = username;
-
+    if(username!="")
+    {
+        document.getElementById("username-input").value = username;
+    }
+    else{
+        document.getElementById("username-input").value = "Enter Username"
+    }
     
 
     let stats = await getLichessStats(username);
-    totalGames = stats.all;
-    gamesWon = stats.win;
-    gamesLost = stats.loss;
-    gamesDrawn = stats.draw;
 
-    document.getElementById("total-games").textContent = "Total Games: " + totalGames;
-    document.getElementById("games-won").textContent = "Games Won: " + gamesWon;
-    document.getElementById("games-lost").textContent = "Games Lost: " + gamesLost;
-    document.getElementById("games-drawn").textContent = "Games Drawn: " + gamesDrawn;
-
-    await chrome.storage.local.set({
-    gameCount: stats.all,
-    winCount: stats.win,
-    lossCount: stats.loss,
-    drawCount: stats.draw
-});
-    updateTree();
+    let winRate = calculateWinRate(stats);
+    updateUI(stats, winRate);
 }
 
 //event listeners
 
-
-/*
-document.getElementById("Won-Button").addEventListener("click", addWin);
-document.getElementById("Lost-Button").addEventListener("click", addLoss);
-document.getElementById("Draw-Button").addEventListener("click",addDraw );
-document.getElementById("Clear-Button").addEventListener("click",clearAll);
-
-*/
 document.addEventListener("DOMContentLoaded", loadUser)
 document.getElementById("username-input").addEventListener("keydown",(event)=>checkKey(event));
